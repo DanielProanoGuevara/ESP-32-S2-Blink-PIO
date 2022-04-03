d52892c78e384f9f9bb49f6c356425f0eef21766
Daniel Proaño <dproanog@est.ups.edu.ec>
2022-04-03 21:43:41 +0100

    Blink test on PIO

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include <Arduino.h>
+#include <WS2812FX.h>
+#include <driver/gpio.h>
+
+#define LED_COUNT 1
+#define LED_PIN GPIO_NUM_18
+
+WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(921600);
   Serial.println("Hello from setup");
+
+  ws2812fx.init();
+  ws2812fx.setBrightness(50);
+  ws2812fx.setSpeed(2000);
+  ws2812fx.setMode(FX_MODE_BLINK_RAINBOW);
+  ws2812fx.start();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello from loop");
-  delay(1000);
+  //Serial.println("Hello from loop");
+  //delay(1);
+  ws2812fx.service();
 }
